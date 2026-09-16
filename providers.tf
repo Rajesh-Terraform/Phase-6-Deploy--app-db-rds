04ab1d7875b811f00f370a688a71be262a561fa3
rajeshjonnalagadda294@gmail.com <rajeshjonnalagadda294@gmail.com>
2026-09-16 15:35:07 +0530

    phase6 app-db-rds apply

--- providers.tf
+++ providers.tf
@@ -1,6 +1,12 @@
 terraform {
   required_version = ">= 1.5.0"
 
+  backend "s3" {
+    bucket = "harish-gaddam-bucket123" 
+    key    = "phase6/terraform.tfstate"
+    region = "ap-south-1"
+  }
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
